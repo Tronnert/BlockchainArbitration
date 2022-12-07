3ab009528e5764846cef93f41c772cc98a3b6b91
King_of_kings <kurov666777@gmail.com>
2022-12-07 23:11:50 +0300

    basic info from binance

--- get_api.py
+++ get_api.py
@@ -1,4 +1,13 @@
 from requests import get
+import json
 
 
-print(get("https://api.coingecko.com/api/v3/ping").json())
+def only_hot(one):
+    return one["count"] >=10000
+
+
+# print(json.dumps(get("https://api.binance.com/api/v3/exchangeInfo", params={"symbol": "ETHBTC"}).json(), indent=4))
+with open("24.json", mode="w") as file:
+    answer = list(filter(only_hot, get("https://api.binance.com/api/v3/ticker/24hr").json()))
+    print(len(answer))
+    file.write(json.dumps(answer, indent=4))
