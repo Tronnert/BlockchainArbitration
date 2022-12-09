6b2b726a2210b0b5298915ec2aace002e60b2e0b
arseniy.kolomeytsev@gmail.com <arseniy.kolomeytsev@gmail.com>
2022-12-09 23:37:35 +0300

    вроде ботает

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from binance_websocket import BinanceWebsocket
+from poloniex_websocket import PoloniexWebsocket
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     binancewebsocket = BinanceWebsocket()
+    poloniexWebsocket = PoloniexWebsocket()
+    poloniexWebsocket.start()
     binancewebsocket.start()
--- poloniex_websocket.py
+++ poloniex_websocket.py
@@ -1,4 +1,4 @@
-import websocket
+'''import websocket
 from datetime import datetime
 import time
 import threading
@@ -61,3 +61,49 @@ websocket_thread = threading.Thread(target=run_websocket)
 all_thread = [schedule_thread, websocket_thread]
 schedule_thread.start()
 websocket_thread.start()
+
+'''
+import websocket
+from datetime import datetime
+import time
+import threading
+import schedule
+import json
+from consts import GLOBAL_OUTPUT_FILE_NAME
+
+class PoloniexWebsocket():
+    def __init__(self) -> None:
+        self.resent = dict()
+        self.schedule_thread = threading.Thread(target=self.scheduling)
+        self.websocket_thread = threading.Thread(target=self.run_websocket)
+
+    def start(self) -> None:
+        self.schedule_thread.start()
+        self.websocket_thread.start()
+
+    def job(self) -> None:
+        now_time = int(datetime.utcnow().timestamp())
+        with open(GLOBAL_OUTPUT_FILE_NAME, mode="a") as file:
+            if 'event' not in self.resent.keys():
+                for e in self.resent.values():
+                    print("\t".join(map(str, (now_time, *e))), file=file)
+
+    def scheduling(self) -> None:
+        schedule.every(1).seconds.do(self.job)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    def on_open(self, ws) -> None:
+        print("ON OPEN")
+        ws.send(open("poloniex.json").read())
+
+    def on_message(self, ws, mess):
+        mess = json.loads(mess)
+        self.resent[mess["data"][0]["symbol"]] = (*mess["data"][0]["symbol"].split('_'), "poloniex", float(mess["data"][0]["bids"][0][0]), float(mess["data"][0]["asks"][0][0]))
+
+    def run_websocket(self) -> None:
+        print("START")
+        wss = f"wss://ws.poloniex.com/ws/public"
+        wsa = websocket.WebSocketApp(wss, on_message=self.on_message, on_open=self.on_open)
+        wsa.run_forever()
