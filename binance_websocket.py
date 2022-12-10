6a2730ca0836469386eeea73f87c5c2357e06dac
tronnert <dima.golovanov.2005@mail.ru>
2022-12-10 11:51:55 +0300

    fuck_you_shedule

--- binance_websocket.py
+++ binance_websocket.py
@@ -1,19 +1,17 @@
 import websocket
 from datetime import datetime
-import time
 import threading
-import schedule
 import json
 from consts import GLOBAL_OUTPUT_FILE_NAME
 
 class BinanceWebsocket():
     def __init__(self) -> None:
         self.resent = dict()
-        self.schedule_thread = threading.Thread(target=self.scheduling)
+        # self.schedule_thread = threading.Thread(target=self.scheduling)
         self.websocket_thread = threading.Thread(target=self.run_websocket)
 
     def start(self) -> None:
-        self.schedule_thread.start()
+        # self.schedule_thread.start()
         self.websocket_thread.start()
 
     def job(self) -> None:
@@ -22,11 +20,12 @@ class BinanceWebsocket():
             for e in self.resent.values():
                 print("\t".join(map(str, (now_time, *e))), file=file)
 
-    def scheduling(self) -> None:
-        schedule.every(1).seconds.do(self.job)
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+    # def scheduling(self) -> None:
+    #     #schedule.every(1).seconds.do(self.job)
+    #     now = datetime.ut
+    #     while True:
+    #         schedule.run_pending()
+    #         time.sleep(1)
 
     def on_open(self, ws) -> None:
         print("ON OPEN")
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from get_top100pairs import get_top100pars
+from sheduler import Sheduler
 from binance_websocket import BinanceWebsocket
 
 
-if __name__ == "main":
+if __name__ == "__main__":
+    top100pairs = get_top100pars()
     binancewebsocket = BinanceWebsocket()
     binancewebsocket.start()
+    sheduler = Sheduler(binancewebsocket)
+    sheduler.start()
+    
