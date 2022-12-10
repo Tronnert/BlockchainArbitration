c0aad692eefb0634be829430b162d9e1a6353486
arseniy.kolomeytsev@gmail.com <arseniy.kolomeytsev@gmail.com>
2022-12-10 12:49:40 +0300

    замена вроде работает

--- kraken_websocket.py
+++ kraken_websocket.py
@@ -19,8 +19,13 @@ class KrakenWebsocket(BinanceWebsocket):
 
     def on_message(self, ws, mess):
         mess = json.loads(mess)
+        cur1, cur2 = mess[3].split('/')
+        if cur1 in self.different_names.keys():
+            cur1 = self.different_names[cur1]
+        if cur2 in self.different_names.keys():
+            cur2 = self.different_names[cur2]
         if 'as' in mess[1].keys():
-            self.resent[mess[3]] = (*mess[3].split('/'), "kraken", float(mess[1]["bs"][0][0]), float(mess[1]["as"][0][0]))
+            self.resent[mess[3]] = (cur1, cur2, "kraken", float(mess[1]["bs"][0][0]), float(mess[1]["as"][0][0]))
         elif 'a' in mess[1].keys():
             self.resent[mess[3]] = (*self.resent[mess[3]][:-1], float(mess[1]["a"][0][0]))
         elif 'b' in mess[1].keys():
