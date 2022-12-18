f40edd4077c2e3793b6350c37755143116463a62
Ivan <st.affeev@yandex.ru>
2022-12-18 16:37:17 +0300

    consts fixed

--- consts.py
+++ consts.py
@@ -4,29 +4,32 @@ DIFFERENT_NAMES_FILE_NAME = "json/different_names.json"
 
 BINANCE_SUB_FILE = "json/binance_sub.json"
 BINANCE_STREAM_NAME = "wss://stream.binance.com:9443/ws/tronnert_stream"
+
+POLONIEX_SUB_FILE = "json/poloniex_sub.json"
+POLONIEX_STREAM_NAME = "wss://ws.poloniex.com/ws/public"
+
+KRAKEN_SUB_FILE = "json/kraken_sub.json"
+KRAKEN_STREAM_NAME = "wss://ws.kraken.com"
+
+GATES_IO_STREAM_NAME = "wss://api.gateio.ws/ws/v4/"
+GATES_IO_SUB_FILE = "json/gate_io_sub.json"
+
+HUOBI_SUB_FILE = "json/huobi_sub.json"
+HUOBI_STREAM_NAME = "wss://api.huobi.pro/ws"
+
 BINANCE_SYMBOLS = "https://api.binance.com/api/v3/exchangeInfo"
 BINANCE_TICKER = "https://api.binance.com/api/v3/ticker/24hr"
 BINANCE_MAX_SYMBOLS = 300
 BINANCE_STEP = 50
 
-POLONIEX_SUB_FILE = "json/poloniex_sub.json"
-POLONIEX_STREAM_NAME = "wss://ws.poloniex.com/ws/public"
 POLONIEX_TICKER = "https://api.poloniex.com/markets/ticker24h"
 POLONIEX_MAX_SYMBOLS = 300
 
-KRAKEN_SUB_FILE = "json/kraken_sub.json"
-KRAKEN_STREAM_NAME = "wss://ws.kraken.com"
 KRAKEN_SYMBOLS = "https://api.kraken.com/0/public/AssetPairs"
 
-
-GATES_IO_STREAM_NAME = "wss://api.gateio.ws/ws/v4/"
-GATES_IO_SUB_FILE = "json/gate_io_sub.json"
 GATES_IO_SYMBOLS = "https://api.gateio.ws/api/v4/spot/currency_pairs"
 GATES_IO_TICKER = "https://api.gateio.ws/api/v4/spot/tickers"
 GATES_IO_MAX_SYMBOLS = 300
 
-HUOBI_SUB_FILE = "json/huobi_sub.json"
-HUOBI_STREAM_NAME = "wss://api.huobi.pro/ws"
 HUOBI_SYMBOLS = "https://api.huobi.pro/v2/settings/common/symbols"
 HUOBI_MAX_SYMBOLS = 300
-
