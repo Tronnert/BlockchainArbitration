ca14b0c7531cf985260fcb69e7ee104998c64753
Ivan <st.affeev@yandex.ru>
2022-12-22 00:25:41 +0300

    kraken fee added

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from sockets.bybit_websocket import BybitWebsocket
 from sockets.bitget_websocket import BitgetWebsocket
 
 if __name__ == "__main__":
-    filename = "test_polx.tsv"
+    filename = "test_krak.tsv"
 
     # to_start = [BinanceWebsocket(), BybitWebsocket(), BitgetWebsocket(),
     #             PoloniexWebsocket(), GateWebsocket(), HuobiWebsocket(),
     #             KrakenWebsocket()]
-    to_start = [PoloniexWebsocket()]
+    to_start = [KrakenWebsocket()]
     [socket.start() for socket in to_start]
     scheduler = Scheduler(*to_start, filename=filename)
     scheduler.start()
--- sockets/kraken_websocket.py
+++ sockets/kraken_websocket.py
@@ -13,21 +13,31 @@ class KrakenWebsocket(BaseWebsocket):
         resp = get(KRAKEN_SYMBOLS).json()["result"]
         ans = dict()
         for val in resp.values():
-            val = self.rename(val["wsname"].split("/"))
-            ans["".join(val)] = val
+            ans[val["wsname"]] = (*self.rename(val["wsname"].split("/")),
+                                  list(map(lambda x: (float(x[0]), float(x[1])), val["fees"])))
         return ans
+
+    def define_fee(self, symb: str, vol) -> float:
+        """Возвращает комиссию, соответствующую объему ордера"""
+        vol = float(vol)
+        fees = self.list_of_symbols[symb][-1]
+        for i in range(len(fees) - 1):
+            if fees[i][0] <= vol <= fees[i + 1][0]:
+                return fees[i][1] / 100
 
     def made_sub_json(self) -> dict:
         """Создание параметров соединения"""
         sub_json = super().made_sub_json()
-        sub_json["pair"] = list(map(lambda x: "/".join(x), self.list_of_symbols.values()))
+        symbs = [i[:-1] for i in self.list_of_symbols.values()]
+        sub_json["pair"] = list(map(lambda x: "/".join(x), symbs))
         return sub_json
 
-    def process(self, message: dict) -> None:
+    def process(self, message: list) -> None:
         """Обработка данных"""
-        cur1, cur2 = map(lambda x: self.different_names.get(x, x),
-                         message[3].split('/'))
-        symb = cur1 + cur2
+        if isinstance(message, dict):
+            return
+        symb = message[3]
+        cur1, cur2 = self.list_of_symbols[symb][:-1]
         if 'as' in message[1].keys():
             bids, asks = message[1]["bs"], message[1]["as"]
             if not bids:
@@ -36,7 +46,9 @@ class KrakenWebsocket(BaseWebsocket):
                 asks = [[0, 0]]
             self.resent[symb] = (
                 cur1, cur2, "kraken", float(bids[0][0]), float(bids[0][1]),
-                float(asks[0][0]), float(asks[0][1])
+                self.define_fee(symb, bids[0][1]),
+                float(asks[0][0]), float(asks[0][1]),
+                self.define_fee(message[3], asks[0][1])
             )
         else:
             if 'a' in message[1].keys():
@@ -44,14 +56,15 @@ class KrakenWebsocket(BaseWebsocket):
                 if not asks:
                     asks = [[0, 0]]
                 self.resent[symb] = (
-                    *self.resent[message[3]][:-1], float(asks[0][0]),
-                    float(asks[0][1])
+                    *self.resent[message[3]][:-3], float(asks[0][0]),
+                    float(asks[0][1]), self.define_fee(symb, asks[0][1])
                 )
             if 'b' in message[1].keys():
                 bids = message[1]["b"]
                 if not bids:
                     bids = [[0, 0]]
                 self.resent[symb] = (
-                    *self.resent[message[3]][:-2], float(bids[0][0]),
-                    float(bids[0][1]), self.resent[message[3]][-1]
+                    *self.resent[message[3]][:3], float(bids[0][0]),
+                    float(bids[0][1]), self.define_fee(symb, bids[0][1]),
+                    *self.resent[message[3]][-3:]
                 )
